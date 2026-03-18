cf0dd876150b18bc8333e85b2681d154e3798f7e
mmkhalil2002 <mmkhalil2001@gmail.com>
2026-03-18 13:17:59 -0500

    031826

--- object_detection/python/cifar-18-26-unknown30-cnn-128-256-512-1024-1744s-L5205-A9999-T8766.py
+++ object_detection/python/cifar-18-26-unknown30-cnn-128-256-512-1024-1744s-L5205-A9999-T8766.py
@@ -525,8 +525,8 @@ from torchvision import datasets, transforms
 # ============================================================
 
 MODEL_PATH = "../../../"
-MODEL_FILENAME = "cifar-25-32-unknown30-cnn-128-256-512-1024-1744s-L5205-A9999-T8766"
-DATA_PATH = "../../../data/cifar_25_32_unknown30"
+MODEL_FILENAME = "cifar-18-26-unknown30-cnn-128-256-512-1024-1744s-L5205-A9999-T8766"
+DATA_PATH = "../../../data/cifar_18_26_unknown30"
 
 BATCH_SIZE = 128
 NUM_EPOCHS = 100
